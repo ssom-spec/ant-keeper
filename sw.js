bdd90979d20072a008200843254f6891f4b165c2
ssom-spec <somi0311@gmail.com>
2026-09-05 22:27:10 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 개미 사육 관리 - 앱 셀 캐시용 최소 서비스워커
 // 실제 데이터는 IndexedDB에 저장되며, 이 파일은 오프라인에서도 화면 자체가 열리도록 돕는 역할만 한다.
-const CACHE_NAME = 'antkeeper-shell-v3';
+const CACHE_NAME = 'antkeeper-shell-v4';
 const SHELL_FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
